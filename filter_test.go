6f2308c4353d7f88530bf543f9855c15eba82eeb
mavricknz <mavricknzwork@yahoo.com>
2012-12-13 21:31:45 +0000

    escape/unescape filter values, tests.

--- filter_test.go
+++ filter_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/hex"
 	"github.com/mavricknz/asn1-ber"
 	"testing"
-	//"fmt"
+	"fmt"
 	"bytes"
 )
 
@@ -87,6 +87,28 @@ func TestFilterEncode(t *testing.T) {
 		}
 	}
 }
+
+func TestFilterValueUnescape(t *testing.T) {
+    filter := `cn=abc \(123\) \28bob\29 \\\\ \*`
+    filterStandard := `cn=abc (123) (bob) \\ *`
+    filterUnescaped := UnescapeFilterValue(filter)
+    if filterUnescaped != filterStandard {
+        t.Errorf("Standard and Unescaped filter do not match [%s] != [%s]\n" , filterStandard, filterUnescaped)
+    }
+    fmt.Printf("filter           : %s\n", filter)
+    fmt.Printf("filter Standard  : %s\n", filterStandard)
+    fmt.Printf("filter Unescaped : %s\n", UnescapeFilterValue(filter))
+}
+
+func TestFilterValueEscape(t *testing.T) {
+    filter := "£¥©" + `(*\)`
+    filterStandard := `\a3\a5\a9\28\2a\5c\29`
+    filterEscaped  := EscapeFilterValue(filter)
+    if filterEscaped != filterStandard {
+        t.Errorf("Standard and Escaped filter do not match [%s] != [%s]\n" , filterStandard, filterEscaped)
+    }
+}
+
 func BenchmarkFilterCompile(b *testing.B) {
 	b.StopTimer()
 	filters := make([]string, len(test_filters))
