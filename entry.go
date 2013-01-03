129229e1cea62ee8bf651bfd57467a21a140f696
mavricknz <mavricknzwork@yahoo.com>
2013-01-03 08:39:23 +0000

    Add NewEntry, some helper functions.

--- entry.go
+++ entry.go
@@ -19,23 +19,55 @@ func (req *Entry) RecordType() uint8 {
 	return EntryRecord
 }
 
-func (e *Entry) GetAttributeValues(Attribute string) []string {
+func NewEntry(dn string) *Entry {
+	entry := &Entry{DN: dn}
+	entry.Attributes = make([]*EntryAttribute, 0)
+	return entry
+}
+
+// AddAttributeValue - Add a single Attr value
+// no check is done for duplicate values.
+func (e *Entry) AddAttributeValue(attributeName, value string) {
+	index := e.GetAttributeIndex(attributeName)
+	if index == -1 {
+		eAttr := EntryAttribute{Name: attributeName, Values: []string{value}}
+		e.Attributes = append(e.Attributes, &eAttr)
+	} else {
+		e.Attributes[index].Values = append(e.Attributes[index].Values, value)
+	}
+}
+
+// AddAttributeValues - Add via a name and slice of values
+// no check is done for duplicate values.
+func (e *Entry) AddAttributeValues(attributeName string, values []string) {
+	index := e.GetAttributeIndex(attributeName)
+	if index == -1 {
+		eAttr := &EntryAttribute{Name: attributeName, Values: values}
+		e.Attributes = append(e.Attributes, eAttr)
+	} else {
+		e.Attributes[index].Values = append(e.Attributes[index].Values, values...)
+	}
+}
+
+func (e *Entry) GetAttributeValues(attributeName string) []string {
 	for _, attr := range e.Attributes {
-		if attr.Name == Attribute {
+		if attr.Name == attributeName {
 			return attr.Values
 		}
 	}
-
 	return []string{}
 }
 
-func (e *Entry) GetAttributeValue(Attribute string) string {
-	values := e.GetAttributeValues(Attribute)
-	if len(values) == 0 {
-		return ""
-	}
-	return values[0]
-}
+// GetAttributeValue - returning an empty string is a bad idea
+// some directory servers will return empty attr values (Sunone).
+// Just asking for trouble. 
+//func (e *Entry) GetAttributeValue(attributeName string) string {
+//	values := e.GetAttributeValues(attributeName)
+//	if len(values) == 0 {
+//		return ""
+//	}
+//	return values[0]
+//}
 
 func (e *Entry) GetAttributeIndex(Attribute string) int {
 	for i, attr := range e.Attributes {
