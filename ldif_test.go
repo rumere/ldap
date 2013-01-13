cdd202d35d5d845d24ae347e639f50ba499fc68a
mavricknz <mavricknzwork@yahoo.com>
2013-01-13 21:10:06 +0000

    minor fixes to tests.

--- ldif_test.go
+++ ldif_test.go
@@ -51,6 +51,7 @@ func TestLDIFOpenAndRead(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
+
 	// record 0
 	fmt.Printf("Reading record 0\n")
 	record, err := lr.ReadLDIFEntry()
@@ -65,9 +66,10 @@ func TestLDIFOpenAndRead(t *testing.T) {
 		t.Errorf("record 0: description mismatch")
 	}
 	fmt.Printf("0 (entry): DN: %s\n", entry.DN)
+
 	// record 1
 	fmt.Printf("Reading record 1\n")
-	LDIFDebug = true
+	//LDIFDebug = true
 	record, err = lr.ReadLDIFEntry()
 	if err != nil {
 		t.Errorf(err.Error())
@@ -76,10 +78,12 @@ func TestLDIFOpenAndRead(t *testing.T) {
 		t.Errorf("record 1: record.RecordType() mismatch")
 	}
 	entry = record.(*Entry)
-	if entry.GetAttributeValues("description")[0] != "a multi-line attribute value" {
+	if entry.GetAttributeValues("description")[0] != "This text was originally base64 encoded." {
 		t.Errorf("record 1: description mismatch")
 	}
-	LDIFDebug = false
+	fmt.Printf("1 (entry): DN: %s\n", entry.DN)
+	//LDIFDebug = false
+
 	// record 2
 	fmt.Printf("Reading record 2\n")
 	record, err = lr.ReadLDIFEntry()
@@ -92,6 +96,7 @@ func TestLDIFOpenAndRead(t *testing.T) {
 	modRequest := record.(*ModifyRequest)
 	fmt.Printf("2 (ModifyRequest): DN: %s\n", modRequest.DN)
 	fmt.Printf(modRequest.DumpModRequest())
+
 	// record 3
 	fmt.Printf("Reading record 3\n")
 	record, err = lr.ReadLDIFEntry()
@@ -103,6 +108,7 @@ func TestLDIFOpenAndRead(t *testing.T) {
 	}
 	addRequest := record.(*AddRequest)
 	fmt.Printf("3 (addRequest): DN: %s\n", addRequest.Entry.DN)
+
 	// record 4
 	fmt.Printf("Reading record 4\n")
 	record, err = lr.ReadLDIFEntry()
@@ -114,6 +120,7 @@ func TestLDIFOpenAndRead(t *testing.T) {
 	}
 	deleteRequest := record.(*DeleteRequest)
 	fmt.Printf("3 (deleteRequest): DN: %s\n", deleteRequest.DN)
+
 	// nil record
 	fmt.Printf("Reading record 5 (nil)\n")
 	record, err = lr.ReadLDIFEntry()
