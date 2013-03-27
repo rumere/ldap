c22225aa902cd6b44a4eeb739a94fa52bbd16a7c
mavricknz <mavricknzwork@yahoo.com>
2013-03-27 22:48:52 +0000

    DumpModRequest() -> String()

--- modify.go
+++ modify.go
@@ -121,7 +121,7 @@ func NewModifyRequest(dn string) (req *ModifyRequest) {
 }
 
 // Basic LDIF dump, no formating, etc
-func (req *ModifyRequest) DumpModRequest() (dump string) {
+func (req *ModifyRequest) String() (dump string) {
 	dump = fmt.Sprintf("dn: %s\n", req.DN)
 	dump = fmt.Sprintf("changetype: modify\n")
 	for _, mod := range req.Mods {
